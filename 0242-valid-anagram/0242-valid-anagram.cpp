a5ea2f74fc85eef112c9ab3debc78c6676a735d1
Abhinav Gorantla <abhinav20016@gmail.com>
2023-07-09 22:45:33 +0530

    Time: 16 ms (41.96%), Space: 7.1 MB (99.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> m;
-        
-        for(int i = 0; i < s.size(); i++)
-            m[s[i]] = 0;
-        
-        for(int i = 0; i < s.size(); i++)
-            m[s[i]]++;
-        
-        for(int i = 0; i < t.size(); i++){
-            if(m[t[i]] <= 0)
-                return false;
-            
-            m[t[i]]--;
-        }
-        
-        int sum=0;
-        for(auto i: m){
-            sum+=i.second;
-        }
-        
-        if(sum == 0)
-            return true;
-        return false;
-            
+        sort(begin(s), end(s));
+        sort(begin(t), end(t));
+        return (s==t);
     }
 };
