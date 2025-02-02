03b11fbde0db1c0d587e0b0ee54b787a410ef8d3
Abhinav Gorantla <abhinav20016@gmail.com>
2025-02-02 11:37:30 -0700

    Time: 0 ms (100%), Space: 27.7 MB (61.84%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         if(nums.size() == 1) return true;
+        vector<bool> res;
 
         for(int i = 0; i < nums.size() - 1; i++) {
             if(nums[i]%2 != nums[i + 1]%2) {
-                continue;
+                res.push_back(true);
             } else {
-                return false;
+                res.push_back(false);
             }
         }
 
+        for(bool a: res) {
+            if(!a) {
+                return false;
+            }
+        }
         return true;
     }
 };
