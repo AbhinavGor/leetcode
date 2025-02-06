ec3ceff1647bc66ef4ca727759dcd250c879906b
Abhinav Gorantla <abhinav20016@gmail.com>
2025-02-05 22:31:38 -0700

    Time: 1770 ms (5.01%), Space: 435.3 MB (6.27%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, set<pair<int, int>>> m;
+
+        for(int i = 0; i < nums.size() - 1; i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                m[nums[i] * nums[j]].insert({nums[i], nums[j]});
+            }
+        }
+
+        int res = 0;
+
+        for(auto i: m) {
+            if(i.second.size() > 1) {
+                res += fib(i.second.size())*8;
+            }
+        }
+
+        return res;
+    }
+
+private:
+    int fib(int n) {
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            res += i;
+        }
+
+        return res;
+    }
+};
