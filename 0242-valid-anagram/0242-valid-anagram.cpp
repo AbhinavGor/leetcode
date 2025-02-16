0ce378624bb81cdbec83a27c026462d00843e3fc
Abhinav Gorantla <abhinav20016@gmail.com>
2025-02-16 11:49:17 -0700

    Time: 2 ms (60.46%), Space: 9.9 MB (26.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
-        return (s==t);
+        unordered_map<char, int> m1, m2;
+        for(char c: s) {
+            m1[c] += 1;
+        }
+
+        for(char c: t) {
+            m2[c] += 1;
+        }
+
+        for(auto i: m1) {
+            if(m2[i.first] != i.second) {
+                return false;
+            } else {
+                m1[i.first] = -1;
+                m2[i.first] = -1;
+            }
+        }
+
+        for(auto i: m1) {
+            if(i.second != -1) return false;
+        }
+
+        for(auto i: m2) {
+            if(i.second != -1) return false;
+        }
+
+        return true;
     }
 };
