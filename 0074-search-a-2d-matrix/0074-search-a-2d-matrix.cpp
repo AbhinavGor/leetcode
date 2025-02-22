e2f0a60dc08301c8f717aaef7a583a8e553a3c0e
Abhinav Gorantla <abhinav20016@gmail.com>
2025-02-22 16:44:26 -0700

    Time: 0 ms (100%), Space: 13.4 MB (43.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,36 +1,50 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int t = 0, b = matrix.size() - 1, l = 0, r = matrix[0].size() - 1;
-        return binSearch(matrix, t, b, l, r, target);        
-    }
-    
-    bool binSearch(vector<vector<int>>& matrix, int t, int b, int l, int r, int x){
-        if(b >= t){
-            int mid = t + (b - t)/2;
-            if(matrix[mid][0] <= x && matrix[mid][r] >= x){
-                return tSearch(matrix[mid], l, r, x);
-            }else if(matrix[mid][0] > x){
-                return binSearch(matrix, t, mid - 1, l, r, x);
-            }else if(matrix[mid][r] < x){
-                return binSearch(matrix, mid + 1, b, l, r, x);
+        // find row
+        int p = matrix.size(), q = matrix[0].size();
+        int t = 0, b = p - 1;
+        int m = t + (b-t)/2;
+
+        while(t < b) {
+            if(matrix[m][0] <= target && target <= matrix[m][q-1]) {
+                break;
+            } else if(matrix[m][0] > target) {
+                // search top rows
+                b = m - 1;
+            } else {
+                // search bottom rows
+                t = m + 1;
             }
-        }
-        return false;
-    }
-    
-    bool tSearch(vector<int>& nums, int l, int r, int x){
-        if(r >= l){
-            int mid = l + (r - l)/2;
-            
-            if(nums[mid] == x)
-                return true;
-            if(nums[mid] < x)
-                return tSearch(nums, mid + 1, r, x);
-            if(nums[mid] > x)
-                return tSearch(nums, l, mid - 1, x);
+            m = t + (b - t)/2;
         }
         
+        int target_row = m;
+        cout<<m<<" ";
+        vector<int> t_row = matrix[m];
+
+        int l = 0, r = q - 1;
+        m = l + (r-l)/2;
+
+        if(t_row[m] == target || t_row[l] == target || t_row[r] == target) {
+                return true;
+        }
+
+        // find col
+        while(l < r) {
+            if(t_row[m] == target || t_row[l] == target || t_row[r] == target) {
+                return true;
+            }
+
+            if(t_row[m] < target) {
+                l = m + 1;
+            } else if(t_row[m] > target) {
+                r = m-1;
+            }
+            m = l + (r-l)/2;
+        }
+
         return false;
+        
     }
 };
