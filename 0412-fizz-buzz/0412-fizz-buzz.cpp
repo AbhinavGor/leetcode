abbd35103b5854c254bb56ffc35276ca4da9d7b8
Abhinav Gorantla <abhinav20016@gmail.com>
2025-03-17 19:53:58 -0700

    Time: 0 ms (100%), Space: 11.7 MB (44.01%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> res(n);
-        for(int i = 1; i <= n; i++){
-            res[i - 1] = to_string(i);
+        vector<string> res;
+        for(int i = 1; i < n+1; i++) {
+            if(i%15==0){
+                res.push_back("FizzBuzz");
+            } else if(i%5==0) {
+                res.push_back("Buzz");
+            } else if(i%3==0) {
+                res.push_back("Fizz");
+            } else {
+                res.push_back(to_string(i));
+            }
         }
-        
-        for(int i = 2; i < n; i+=3)
-            res[i] = "Fizz";
-        
-        for(int i = 4; i < n; i+=5)
-            res[i] = "Buzz";
-        
-        for(int i = 14; i < n; i+=15)
-            res[i] = "FizzBuzz";
-        
+
         return res;
     }
 };
