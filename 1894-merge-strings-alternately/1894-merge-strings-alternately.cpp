8552ea8a9ef31db30b6d4e6c76e7bc397f850ada
Abhinav Gorantla <abhinav20016@gmail.com>
2025-03-17 19:05:08 -0700

    Time: 4 ms (30.59%), Space: 8.4 MB (83.39%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    string mergeAlternately(string w1, string w2) {
-        int n1 = w1.size()-1, n2 = w2.size()-1;
-        int a = 0, b = 0;
+    string mergeAlternately(string word1, string word2) {
         string res = "";
+        int w1 = word1.size(), w2 = word2.size();
+        int k = w1 + w2 - 1;
+        int a = 0, b = 0;
 
-        while(a <= n1 && b <= n2) {
-            res += w1[a];
-            res += w2[b];
-            a+=1;
-            b+=1;
+        for(int i = 0; i <= k; i++) {
+            if(i%2 == 0) {
+                if(a < w1) {
+                    res += word1[a];
+                    a++;
+                }
+            } else {
+                if(b < w2) {
+                    res += word2[b];
+                    b++;
+                }
+            }
+        }
+        
+        while(a < w1) {
+            res += word1[a];
+            a++;
         }
 
-        if(a <= n1) {
-            while(a <= n1) {
-                res += w1[a];
-                a+=1;
-            }
-        } 
-        cout<<b<<" "<<n2<<endl;
-        if(b <= n2) {
-            while(b <= n2) {
-                res += w2[b];
-                b+=1;
-            }
+        while(b < w2) {
+            res += word2[b];
+            b++;
         }
 
         return res;
