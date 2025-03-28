91f6ae9ba233947c6ef5519858703e151ec4b64e
Abhinav Gorantla <abhinav20016@gmail.com>
2025-03-28 00:33:01 -0700

    Time: 11 ms (74.83%), Space: 18.4 MB (16.55%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -3,16 +3,11 @@ class Solution:
         col = set()
         posD = set()
         negD = set()
-
+        board = [['.']*n for i in range(n)]
         res = []
-        board = [['.'] * n for i in range(n)]
-
         def backtrack(r):
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return res
-            
+                res.append(["".join(row) for row in board])
             for c in range(n):
                 if c in col or (r+c) in posD or (r-c) in negD:
                     continue
@@ -20,12 +15,11 @@ class Solution:
                 posD.add(r+c)
                 negD.add(r-c)
                 board[r][c] = 'Q'
-
                 backtrack(r+1)
-                
+                board[r][c] = '.'
                 col.remove(c)
                 posD.remove(r+c)
                 negD.remove(r-c)
-                board[r][c] = '.'
+
         backtrack(0)
         return res
