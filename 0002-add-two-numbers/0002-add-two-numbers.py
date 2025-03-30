c04284fc099c9b292d05f76b79149f9ea826d03d
Abhinav Gorantla <abhinav20016@gmail.com>
2025-03-30 10:59:01 -0700

    Time: 0 ms (100%), Space: 18 MB (14.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,29 +7,45 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         res = ListNode(0)
         res_copy = res
-
         carry = 0
 
-        while l1 or l2:
-            if l1:
-                res_copy.val += l1.val
-                l1 = l1.next
-            if l2:
-                res_copy.val += l2.val
-                l2 = l2.next
-            
-            res_copy.val += carry
-
+        while l1 and l2:
+            res_copy.val = l1.val + l2.val + carry
             carry = 0
-
             if res_copy.val >= 10:
                 carry = res_copy.val//10
-                res_copy.val %= 10            
-
-            if l1 != None or l2 !=None:
+                res_copy.val %= 10
+            l1 = l1.next
+            l2 = l2.next
+            if l1 or l2:
                 res_copy.next = ListNode(0)
                 res_copy = res_copy.next
-            elif carry:
-                res_copy.next = ListNode(carry)
-            
-        return res    
+
+        while l1:
+            res_copy.val = carry + l1.val
+            l1 = l1.next
+            carry = 0
+            if res_copy.val >= 10:
+                carry = res_copy.val//10
+                res_copy.val %= 10
+            if l1:
+                res_copy.next = ListNode(0)
+                res_copy = res_copy.next
+        
+        while l2:
+            res_copy.val = carry + l2.val
+            l2 = l2.next
+            carry = 0
+            if res_copy.val >= 10:
+                carry = res_copy.val//10
+                res_copy.val %= 10
+            if l2:
+                res_copy.next = ListNode(0)
+                res_copy = res_copy.next
+        
+        if carry:
+            # if res_copy:
+            res_copy.next = ListNode(carry)
+            # else:
+            #     res_copy = 
+        return res
