1754298ff1479428e49d4341dbd28053ca4c2712
Abhinav Gorantla <abhinav20016@gmail.com>
2025-05-22 20:02:37 -0700

    Time: 4 ms (55.33%), Space: 17.9 MB (35.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,47 +5,49 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = ListNode(0)
-        res_copy = res
-        carry = 0
+        head = ListNode()
+        res = head
+        carry = curr = 0
 
         while l1 and l2:
-            res_copy.val = l1.val + l2.val + carry
-            carry = 0
-            if res_copy.val >= 10:
-                carry = res_copy.val//10
-                res_copy.val %= 10
+            curr = l1.val + l2.val + carry
+            carry = curr//10
+            curr = curr%10
+            head.val = curr
+
             l1 = l1.next
             l2 = l2.next
-            if l1 or l2:
-                res_copy.next = ListNode(0)
-                res_copy = res_copy.next
 
+            if l1 or l2:
+                head.next = ListNode()
+                head = head.next
+        
         while l1:
-            res_copy.val = carry + l1.val
+            curr = l1.val + carry
+            carry = curr//10
+            curr = curr%10
+            head.val = curr
+
             l1 = l1.next
-            carry = 0
-            if res_copy.val >= 10:
-                carry = res_copy.val//10
-                res_copy.val %= 10
+
             if l1:
-                res_copy.next = ListNode(0)
-                res_copy = res_copy.next
+                head.next = ListNode()
+                head = head.next
         
         while l2:
-            res_copy.val = carry + l2.val
+            curr = l2.val + carry
+            carry = curr//10
+            curr = curr%10
+            head.val = curr
+
             l2 = l2.next
-            carry = 0
-            if res_copy.val >= 10:
-                carry = res_copy.val//10
-                res_copy.val %= 10
+
             if l2:
-                res_copy.next = ListNode(0)
-                res_copy = res_copy.next
-        
+                head.next = ListNode()
+                head = head.next
+
         if carry:
-            # if res_copy:
-            res_copy.next = ListNode(carry)
-            # else:
-            #     res_copy = 
+            head.next = ListNode()
+            head = head.next
+            head.val = carry
         return res
